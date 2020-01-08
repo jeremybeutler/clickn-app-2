f9ac4b41fed2916d3fb0241e4d62a2017b61fed7
Jeremy (Ubuntu) <jeremy.e.beutler@gmail.com>
2020-01-08 15:17:28 -0700

    Changes to navbar template, added map tab functionality

--- app/app.js
+++ app/app.js
@@ -1,6 +1,11 @@
 import Vue from "nativescript-vue";
-
 import App from "./components/App";
+// Uncommment the following to see NativeScript-Vue output logs
+Vue.config.silent = false;
+Vue.registerElement("Mapbox", () => require("nativescript-mapbox").MapboxView)
+if (application.ios) {
+    GMSServices.provideAPIKey("AIzaSyDXzLk7b0Tm0koULT84E7D9tkLkH3AnuyI");
+}
 
 new Vue({
     render: h => h(App)
--- app/components/Map.vue
+++ app/components/Map.vue
@@ -1,22 +1,161 @@
-<template lang="html">
-    <Page class="page">
+<template>
+    <Page class="page" xmlns:maps="nativescript-google-maps-sdk">
         <ActionBar class="action-bar">
             <Label class="action-bar-title" text="Map"></Label>
         </ActionBar>
 
         <GridLayout class="page-content">
-            <Label class="page-icon fa" text.decode="&#xf279;"></Label>
-            <Label class="page-placeholder" :text="message"></Label>
+            <!-- <Mapbox
+                accessToken="pk.eyJ1IjoiY2xpY2tuIiwiYSI6ImNqdzhyZWZhazF5ZWMzenMwM200d2YzYnEifQ.AJs2nD_1XKA288fboJuFSw"
+                mapStyle="light"
+                hideCompass="true"
+                zoomLevel="10"
+                showUserLocation="true"
+                disableZoom="false"
+                disableRotation="false"
+                disableScroll="false"
+                disableTilt="false"
+                @mapReady="onMapReady($event)"
+                ref="map"
+            >
+            </Mapbox> -->
+            <!-- <maps:mapView
+            /> -->
         </GridLayout>
     </Page>
 </template>
 
 <script>
+import axios from 'axios'
+const geolocation = require("nativescript-geolocation");
+const { Accuracy } = require("tns-core-modules/ui/enums");
+var mapbox = require("nativescript-mapbox");
 export default {
     data: () => {
         return {
-            message: "<!-- Map page content goes here -->"
+            scrollTimer: null,
+            mapCenter: {
+                latitude: null,
+                longitude: null
+            },
+            mapRadius: null,
+            markers: [],
+            markerIDs: []
         };
+    },
+    methods: {
+        async onMapReady(args) {
+            // if no permission was granted previously this will open a user consent screen
+            // await this.$refs.map.nativeView.requestFineLocationPermission();
+            await this.getLocation();
+            await args.map.setCenter({
+                lat: this.mapCenter.latitude,
+                lng: this.mapCenter.longitude,
+                animated: true
+            })
+            let viewport = await args.map.getViewport()
+            this.mapRadius = this.calculateViewportRadius(viewport)
+            // let nearby_events = await axios.post('http://10.0.2.2:3000/events/search', {
+            //     latitude: this.mapCenter.latitude,
+            //     longitude: this.mapCenter.longitude,
+            //     search_radius: this.mapRadius
+            // })
+            let request = await axios.get('http://10.0.2.2:3000/events')
+            let events = request.data.data
+            for (let key in events) {
+                let event = events[key]
+                let marker = {
+                    id: event._id,
+                    lat: event.location.coordinates[1],
+                    lng: event.location.coordinates[0],
+                    title: event.title,
+                    subtitle: event.description,
+                    onTap: marker => console.log("Marker tapped with title: '" + marker.title + "'"),
+                    onCalloutTap: marker => alert("Marker callout tapped with title: '" + marker.title + "'")
+                }
+                this.markers.push(marker)
+                this.markerIDs.push(marker.id)
+            }
+            await args.map.addMarkers(this.markers)
+            // this.setOnScrollListener();
+        },
+        // async setOnScrollListener() {
+        //     let self = this
+        //     this.$refs.map.nativeView.setOnScrollListener((point) => {
+        //         if(self.scrollTimer !== null) {
+        //             clearTimeout(self.scrollTimer);        
+        //         }
+        //         self.scrollTimer = setTimeout(async function() {
+        //             let center = await self.$refs.map.nativeView.getCenter();
+        //             self.mapCenter.latitude = center.lat;
+        //             self.mapCenter.longitude = center.lng;
+        //             self.getNearbyEvents();
+        //         }, 1500);
+        //     });
+        // },
+        async getNearbyEvents() {
+            let new_markers = []
+            let nearby_events = await axios.post('http://10.0.2.2:3000/events/search', {
+                latitude: this.mapCenter.latitude,
+                longitude: this.mapCenter.longitude,
+                search_radius: this.mapRadius
+            })
+            // console.log(nearby_events)
+
+            for (let key in nearby_events.data.events) {
+                let event = nearby_events.data.events[key]
+                if (this.markerIDs.includes(event._id) === true) continue;
+
+                let marker = {
+                    id: event._id,
+                    lat: event.location.coordinates[1],
+                    lng: event.location.coordinates[0],
+                    title: event.title,
+                    subtitle: event.description
+                }
+                console.log(marker)
+                new_markers.push(marker)
+                this.markers.push(marker)
+                this.markerIDs.push(marker.id)
+            }
+            await this.$refs.map.nativeView.addMarkers(new_markers)
+        },
+        async getLocation() {
+            return new Promise(resolve => {
+                geolocation.enableLocationRequest();
+                geolocation
+                .getCurrentLocation({
+                    desiredAccuracy: Accuracy.high,
+                    maximumAge: 5000,
+                    timeout: 20000
+                })
+                .then(res => {
+                    this.mapCenter.latitude = res.latitude
+                    this.mapCenter.longitude = res.longitude
+                    // console.log(res)
+                    resolve(res)
+                    // this.speed = res.speed;
+                });
+            })
+        },
+        calculateViewportRadius(viewport) {
+            var R = 6371e3; // metres
+            var φ1 = this.toRadians(viewport.bounds.south);
+            var φ2 = this.toRadians(viewport.bounds.north);
+            var Δφ = this.toRadians(viewport.bounds.north - viewport.bounds.south);
+            var Δλ = this.toRadians(viewport.bounds.east - viewport.bounds.west);
+
+            var a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+                    Math.cos(φ1) * Math.cos(φ2) *
+                    Math.sin(Δλ/2) * Math.sin(Δλ/2);
+            var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+            var d = R * c;
+            return (d / 2);
+        },
+        toRadians(degrees) {
+            var pi = Math.PI;
+            return degrees * (pi/180);
+        }
     }
 }
 </script>
